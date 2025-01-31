914481f50247ed168a549b7f1acebffba2be0fa0
LUGIA <79367636+LSZTM@users.noreply.github.com>
2025-02-01 02:29:15 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -478,12 +478,10 @@ def quiz_taker():
             certificate_button = st.button("Generate Certificate")
             emails = s.get_receiver_add()
             selected_email = st.selectbox("Select your email for the certificate:", emails)
-            if certificate_button:
-                # List of names available for certificates, taken from the leaderboard
-                names = [entry["username"] for entry in st.session_state["leaderboard"]]
-                selected_name = st.selectbox("find your name:", names)
-                if selected_name and selected_email:
-                    
+            names = [entry["username"] for entry in st.session_state["leaderboard"]]
+            selected_name = st.selectbox("find your name:", names)
+            if selected_name and selected_email:
+                 if certificate_button:
                     certificate = s.get_certificate(selected_email)
                     s.send_mail(certificate,selected_email)
                     st.success(f"Certificate generated for {selected_name}!")
